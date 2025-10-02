68fa1a43de587fb6d5e0022c657b69006b0876aa
Muhammad Shafiq <engr.laravel@gmail.com>
2025-10-02 15:17:21 +0400

    fixing

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,6 @@ class Settings(BaseSettings):
     AZURE_STORAGE_CONNECTION_STRING: str
     OPENAI_API_KEY:str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440  # Default to 30 minutes if not set
-    SLACK_DESIGN_WEBHOOK_URL: str
     SLACK_WEBHOOK_URL: str
     BASE_URL: str = "http://127.0.0.1:8000/"  # Default base URL
 
